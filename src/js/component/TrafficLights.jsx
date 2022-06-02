d5b2e6063f524d380925d860a70c4bf63f51f5a2
Aleisgar <aleisgar@gmail.com>
2022-06-02 21:28:20 +0000

    first commit

--- src/js/component/TrafficLights.jsx
+++ src/js/component/TrafficLights.jsx
@@ -5,29 +5,59 @@ const TrafficLights = () => {
 	const [yellowLight, setColorYellow] = useState("");
 	const [greenLight, setColorGreen] = useState("");
 
+	let colorCycle = "";
+	const iluminaRojo = () => {
+		redLight === "" ? setColorRed("Resplandor") : setColorRed("");
+	};
+	const iluminaAmarillo = () => {
+		yellowLight === "" ? setColorYellow("Resplandor") : setColorYellow("");
+	};
+	const iluminaVerde = () => {
+		greenLight === "" ? setColorGreen("Resplandor") : setColorGreen("");
+	};
+
+	const myFunction = () => {
+		colorCycle = setInterval(iluminaRojo, 1000);
+		colorCycle = setInterval(iluminaAmarillo, 2000);
+		colorCycle = setInterval(iluminaVerde, 3000);
+	};
 	return (
-		<div className="container">
-			<div
-				className={"red " + redLight}
-				onClick={() => {
-					setColorRed("Resplandor");
-					setColorYellow("");
-					setColorGreen("");
-				}}></div>
-			<div
-				className={"yellow " + yellowLight}
-				onClick={() => {
-					setColorRed("");
-					setColorYellow("Resplandor");
-					setColorGreen("");
-				}}></div>
-			<div
-				className={"green " + greenLight}
-				onClick={() => {
-					setColorRed("");
-					setColorYellow("");
-					setColorGreen("Resplandor");
-				}}></div>
+		<div className="wrapper">
+			<div className="container">
+				<div
+					className={"red " + redLight}
+					onClick={() => {
+						setColorRed("Resplandor");
+						setColorYellow("");
+						setColorGreen("");
+					}}></div>
+				<div
+					className={"yellow " + yellowLight}
+					onClick={() => {
+						setColorRed("");
+						setColorYellow("Resplandor");
+						setColorGreen("");
+					}}></div>
+				<div
+					className={"green " + greenLight}
+					onClick={() => {
+						setColorRed("");
+						setColorYellow("");
+						setColorGreen("Resplandor");
+					}}></div>
+			</div>
+			<button
+				className="btnCycle"
+				onClick={
+					myFunction //para no tener que escribir cada una de las funciones tantas veces
+					//setColorRed("Resplandor")
+					// setColorRed(""),
+					// 	setColorYellow("Resplandor"),
+					// 	setColorYellow(""),
+					// 	setColorGreen("Resplandor");
+				}>
+				Click me
+			</button>
 		</div>
 	);
 };
