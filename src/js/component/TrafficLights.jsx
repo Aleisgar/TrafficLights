0d3ec71d5562320f6f86a079a30ac8647257c85c
Aleisgar <aleisgar@gmail.com>
2022-06-03 09:19:37 +0000

    first commit

--- src/js/component/TrafficLights.jsx
+++ src/js/component/TrafficLights.jsx
@@ -5,22 +5,6 @@ const TrafficLights = () => {
 	const [yellowLight, setColorYellow] = useState("");
 	const [greenLight, setColorGreen] = useState("");
 
-	let colorCycle = "";
-	const iluminaRojo = () => {
-		redLight === "" ? setColorRed("Resplandor") : setColorRed("");
-	};
-	const iluminaAmarillo = () => {
-		yellowLight === "" ? setColorYellow("Resplandor") : setColorYellow("");
-	};
-	const iluminaVerde = () => {
-		greenLight === "" ? setColorGreen("Resplandor") : setColorGreen("");
-	};
-
-	const myFunction = () => {
-		colorCycle = setInterval(iluminaRojo, 1000);
-		colorCycle = setInterval(iluminaAmarillo, 2000);
-		colorCycle = setInterval(iluminaVerde, 3000);
-	};
 	return (
 		<div className="wrapper">
 			<div className="container">
@@ -46,40 +30,9 @@ const TrafficLights = () => {
 						setColorGreen("Resplandor");
 					}}></div>
 			</div>
-			<button
-				className="btnCycle"
-				onClick={
-					myFunction //para no tener que escribir cada una de las funciones tantas veces
-					//setColorRed("Resplandor")
-					// setColorRed(""),
-					// 	setColorYellow("Resplandor"),
-					// 	setColorYellow(""),
-					// 	setColorGreen("Resplandor");
-				}>
-				Click me
-			</button>
+			
 		</div>
 	);
 };
 
 export default TrafficLights;
-
-// function cambiarColor() {
-// 	color === "Resplandor" ? setColor("") : setColor("Resplandor");
-// }
-// function cambiarColor2() {
-// 	color === "Resplandor" ? setColor("") : setColor("Resplandor");
-// }
-// function cambiarColor3() {
-// 	color === "Resplandor" ? setColor("") : setColor("Resplandor");
-// }
-
-{
-	/* <div className={"light red " + color} onClick={cambiarColor}></div>
-			<div
-				className={"light yellow " + color}
-				onClick={cambiarColor2}></div>
-			<div
-				className={"light green " + color}
-				onClick={cambiarColor3}></div> */
-}
